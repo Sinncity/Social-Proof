bb50815cddcb19b2fca0266c90bb37547572b7f9
igpar <igpar@Tparker85.localdomain>
2022-10-20 05:35:34 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import ReviewCard from './ReviewCard';
+import StarCard from './StarCard';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+       
+       <Header/>
+        <StarCard/>
+        <ReviewCard/>
+
+    </div>
+        </>
+  );
+}
+
+export default App;
---
+++ src/ReviewCard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ReviewCard() {
+  return (
+    <div>🗼🗼🗼🗼🗼🗼</div>
+  )
+}
+

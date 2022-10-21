6094848d5789ec4f8f737c0b3bcee186fd01b375
igpar <igpar@Tparker85.localdomain>
2022-10-21 06:57:42 -0600

    I added the actual content for the header section

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./Header.css"
+
+
+
+
 
 export default function Header() {
   return (
-    <div>🤕🤕🤕🥵🥵🤯😶</div>
+  <>
+    <div className='Header'>10,000+ of our users love our products.</div>
+    <p className='Paragraph'> We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services. Rated 5 Stars in Reviews Rated 5 Stars in Report Guru Rated 5 Stars in BestTech Colton Smith Verified Buyer "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!" Irene Roberts Verified Buyer "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery." Anne Wallace Verified Buyer "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"</p>
+    </>
   )
 }
 
